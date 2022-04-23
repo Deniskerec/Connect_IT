d3aa91d6c0e283eb46dbf5b55b367c74dac5be7a
Denis Kerec <ddeniskerec@gmail.com>
2022-04-23 14:53:20 +0200

    validation
    Username and password validation added ( those fields, cant be empty.)

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,12 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required] //this is a validation
         public string Username { get; set; }
-
+        [Required]
         public string Password { get; set; }
     }
 }
